05fe1ebb2cbe635cc48bbf4235fe0197606bba0d
Amar Jaiswal <amar.j@cawstudios.com>
2020-11-26 14:45:28 +0530

    Edit require message

--- track/utils.py
+++ track/utils.py
@@ -7,7 +7,7 @@ logger = logging.getLogger('interakt')
 def require(name, field, data_type):
     """Require that the named `field` has the right `data_type`"""
     if not isinstance(field, data_type):
-        msg = '{0} must have {1}, got: {2}'.format(name, data_type, field)
+        msg = '{0} must have {1}, got: {2}'.format(name, data_type, type(field))
         raise AssertionError(msg)
 
 
